e29d7a3ae34092cc1ed5fd35b3daffd784328439
Jeremy Hunter <jeremy.w.hunter@gmail.com>
2024-02-12 14:20:30 +1300

    sound playing with card click

--- NativeApp/DisplayCardsGrid.js
+++ NativeApp/DisplayCardsGrid.js
@@ -1,11 +1,50 @@
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native'
+import { Audio } from 'expo-av'
+import { keys } from './KeyCards.js'
 import CardButton from './CardButton.js'
 
 const DisplayCardsGrid = ({ userAnswerSetter, cardsArray }) => {
   function setAnswer(inpt) {
-    console.log('set', inpt)
+    console.log('setAnswer', inpt)
+    let audioSource = findNote(inpt)
+    playNote(audioSource.audioSrc)
+
     userAnswerSetter(inpt)
+  }
+
+  function findNote(inpt) {
+    const result = keys.filter((x) => x.name === inpt)
+    return result[0]
+  }
+
+  const playNote = async (note) => {
+    console.log('start', note)
+    const source = note
+    const status = { volume: 1 }
+    try {
+      const { sound } = await Audio.Sound.createAsync(source, status)
+      await sound.playAsync()
+      setTimeout(() => {
+        setVolume(sound, false)
+      }, 1000)
+    } catch (error) {
+      console.log('Error playing sound:', error)
+    }
+  }
+
+  async function setVolume(sound, up) {
+    console.log('setvol', sound)
+    const fadeOutSpeed = 5000
+    const upDown = up ? 1 : -1
+    const volume = up ? 0 : 1
+    const steps = fadeOutSpeed
+    for (let i = 0; i < steps; i++) {
+      const rate = (1 / steps) * upDown
+      const newVolume = volume + rate * i
+      await sound.setVolumeAsync(newVolume)
+      // console.log(up, upDown, newVolume)
+    }
   }
 
   const firstHalfArray = cardsArray.slice(0, cardsArray.length / 2)
